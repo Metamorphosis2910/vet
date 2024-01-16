a6cd09db826ba78dd96d76918f7b6a8cfa28962c
Metamorphosis2910 <ggurvon@gmail.com>
2024-01-17 02:25:36 +0500

    Хотфикс #2

--- js/js.js
+++ js/js.js
@@ -13,3 +13,22 @@ document.addEventListener('click', function (event) {
     }
   });
   
+
+  document.addEventListener("contextmenu", function (e){
+    e.preventDefault();
+}, false);
+
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+  return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+  return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+  return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+  return false;
+  }
+  }
--- personalpage/js/js.js
+++ personalpage/js/js.js
@@ -28,3 +28,21 @@ $('img[data-enlargeable]').addClass('img-enlargeable').click(function() {
   });
 
   
+  document.addEventListener("contextmenu", function (e){
+    e.preventDefault();
+}, false);
+
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+  return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+  return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+  return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+  return false;
+  }
+  }
