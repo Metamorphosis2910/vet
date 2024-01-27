2d67203140b782445a8dbc4a90ee762b82ad1ab0
Metamorphosis2910 <ggurvon@gmail.com>
2024-01-27 18:26:45 +0500

    Большая переработка

--- js/js.js
+++ js/js.js
@@ -13,27 +13,31 @@ document.addEventListener('click', function (event) {
     }
   });
   
+// Initialization for ES Users
+import { Input, Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Input, Ripple });
 
 
+// document.addEventListener("contextmenu", function (e){
+//     e.preventDefault();
+// }, false);
 
-document.addEventListener("contextmenu", function (e){
-    e.preventDefault();
-}, false);
+// document.onkeydown = function(e) {
+//   if(event.keyCode == 123) {
+//   return false;
+//   }
+//   if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+//   return false;
+//   }
+//   if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+//   return false;
+//   }
+//   if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+//   return false;
+//   }
+//   if(e.ctrlKey && e.keyCode == 'CTRL'.charCodeAt(0)){
+//     return false;
+//   }
+//   }
 
-document.onkeydown = function(e) {
-  if(event.keyCode == 123) {
-  return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
-  return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
-  return false;
-  }
-  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
-  return false;
-  }
-  if(e.ctrlKey && e.keyCode == 'CTRL'.charCodeAt(0)){
-    return false;
-  }
-  }
