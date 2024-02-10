9023a654f6bba168e814a3fdbdfc5d212a8272c9
Metamorphosis2910 <ggurvon@gmail.com>
2024-02-10 20:41:07 +0500

    Редизайн #1
    Часть большого редизайна
    Обновлена страница подачи объявлений (недоработано)

--- podacha/js/js.js
+++ podacha/js/js.js
@@ -1,119 +1,80 @@
-document.addEventListener('click', function (event) {
-  const burgerMenu = document.getElementById('navbarToggleExternalContent');
-  const burgerButton = document.querySelector('.navbar-toggler');
 
-  // Если клик был вне бургер-меню и кнопки, то закрываем меню
-  if (!burgerMenu.contains(event.target) && !burgerButton.contains(event.target)) {
-    const isMenuOpen = burgerMenu.classList.contains('show');
-    
-    if (isMenuOpen) {
-      // Используем jQuery для закрытия бургер-меню
-      $(burgerButton).click();
-    }
-  }
-});
+/* *********** */
+/* Скрипт запрета на использование опрределенных клавиш */
+/* *********** */
 
 
+// document.addEventListener("contextmenu", function (e){
+//   e.preventDefault();
+// }, false);
 
-document.getElementById('fileInput').addEventListener('change', handleFileSelect);
+// document.onkeydown = function(e) {
+//   if(event.keyCode == 123) {
+//   return false;
+//   }
+//   if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+//   return false;
+//   }
+//   if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+//   return false;
+//   }
+//   if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+//   return false;
+//   }
+//   if(e.ctrlKey && e.keyCode == 'CTRL'.charCodeAt(0)){
+//     return false;
+//   }
+//   }
 
-function handleFileSelect(event) {
-  const files = event.target.files;
+/* *********** */
+/* Окончание скрипта */
+/* *********** */
 
-  for (const file of files) {
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function (e) {
-        addPhotoElement(e.target.result);
-      };
-      reader.readAsDataURL(file);
-    }
-  }
-
-  // Clear the input value to allow selecting the same file again
-  event.target.value = '';
+/* *********** */
+/* Начало скрипта textarea */
+/* *********** */
+function toggleAfter(element) {
+  element.parentNode.classList.toggle('active', element === document.activeElement);
 }
 
-function addPhotoElement(imageDataUrl) {
-  const gallery = document.getElementById('gallery');
+function limitTextarea(element, maxLength) {
+  var textWithoutSpaces = element.value.replace(/\s/g, '');
 
-  const photoContainer = document.createElement('div');
-  photoContainer.classList.add('photo');
+  if (textWithoutSpaces.length > maxLength) {
+      var truncatedText = textWithoutSpaces.substring(0, maxLength);
+      element.value = element.value.replace(/\S/g, function (match, offset) {
+          return offset < truncatedText.length ? match : '';
+      });
+  }
+}
 
-  const img = document.createElement('img');
-  img.src = imageDataUrl;
+function blockEnterKey(event) {
+  if (event.key === 'Enter') {
+      event.preventDefault();
+  }
+}
 
-  const deleteButton = document.createElement('div');
-  deleteButton.classList.add('delete-button');
-  deleteButton.innerHTML = '×';
-  deleteButton.addEventListener('click', function () {
-    gallery.removeChild(photoContainer);
+document.addEventListener('DOMContentLoaded', function () {
+  var textareas = document.querySelectorAll('.advice_info-desc textarea');
+  textareas.forEach(function (textarea) {
+      textarea.addEventListener('input', function () {
+          limitTextarea(this, 420);
+      });
+      textarea.addEventListener('keydown', blockEnterKey);
   });
 
-  photoContainer.appendChild(img);
-  photoContainer.appendChild(deleteButton);
-
-  gallery.appendChild(photoContainer);
-}
-
-$(window).on('load', function() {
-  var mainHeight = $('main').height();
-  $('main').css('min-height', mainHeight + 'px');
-});
-
-let photoCount = 0;
-
-function addPhotoElement(imageDataUrl) {
-  if (photoCount >= 3) {
-    // Limit the number of uploaded photos to 3
-    alert('Вы можете загрузить не более 3 фотографий.');
-    return;
-  }
-
-  const gallery = document.getElementById('gallery');
-
-  const photoContainer = document.createElement('div');
-  photoContainer.classList.add('photo');
-
-  const img = document.createElement('img');
-  img.src = imageDataUrl;
-
-
-  const deleteButton = document.createElement('div');
-  deleteButton.classList.add('delete-button');
-  deleteButton.innerHTML = '×';
-  deleteButton.addEventListener('click', function () {
-    gallery.removeChild(photoContainer);
-    photoCount--;
+  var inputWrapper2Textarea = document.querySelector('.inputwrapper2 textarea');
+  inputWrapper2Textarea.addEventListener('input', function () {
+      limitTextarea(this, 20);
   });
 
-  photoContainer.appendChild(img);
-  photoContainer.appendChild(deleteButton);
+  var gorodTextarea = document.querySelector('.gorod');
+  gorodTextarea.addEventListener('input', function () {
+      limitTextarea(this, 30);
+  });
+});
 
-  gallery.appendChild(photoContainer);
-
-  photoCount++;
-}
-
-
-document.addEventListener("contextmenu", function (e){
-  e.preventDefault();
-}, false);
-
-document.onkeydown = function(e) {
-  if(event.keyCode == 123) {
-  return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
-  return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
-  return false;
-  }
-  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
-  return false;
-  }
-  if(e.ctrlKey && e.keyCode == 'CTRL'.charCodeAt(0)){
-    return false;
-  }
-  }
+/* *********** */
+/* Окончание скрипта textarea */
+/* *********** */    
+                         
