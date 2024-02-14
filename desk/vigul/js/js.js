67f73e2073de4851a356bb08eb24b04389d39499
Metamorphosis2910 <ggurvon@gmail.com>
2024-02-14 22:52:04 +0500

    Добавлен список объявлений

--- desk/vigul/js/js.js
+++ desk/vigul/js/js.js
@@ -1,50 +1,22 @@
-function hamDropdown() {
-    document.querySelector(".noidung_dropdown").classList.toggle("hienThi");
-   }
-   
-   window.onclick = function(e) {
-     if (!e.target.matches('.nut_dropdown')) {
-     var noiDungDropdown = document.querySelector(".noidung_dropdown");
-       if (noiDungDropdown.classList.contains('hienThi')) {
-         noiDungDropdown.classList.remove('hienThi');
-       }
-     }
-   }
+//   document.addEventListener("contextmenu", function (e){
+//     e.preventDefault();
+// }, false);
 
-   document.addEventListener('click', function (event) {
-    const burgerMenu = document.getElementById('navbarToggleExternalContent');
-    const burgerButton = document.querySelector('.navbar-toggler');
-  
-    // Если клик был вне бургер-меню и кнопки, то закрываем меню
-    if (!burgerMenu.contains(event.target) && !burgerButton.contains(event.target)) {
-      const isMenuOpen = burgerMenu.classList.contains('show');
-      
-      if (isMenuOpen) {
-        // Используем jQuery для закрытия бургер-меню
-        $(burgerButton).click();
-      }
-    }
-  });
+// document.onkeydown = function(e) {
+//   if(event.keyCode == 123) {
+//   return false;
+//   }
+//   if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+//   return false;
+//   }
+//   if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+//   return false;
+//   }
+//   if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+//   return false;
+//   }
+//   if(e.ctrlKey && e.keyCode == 'CTRL'.charCodeAt(0)){
+//     return false;
+//   }
+//   }
 
-  
-  document.addEventListener("contextmenu", function (e){
-    e.preventDefault();
-}, false);
-
-document.onkeydown = function(e) {
-  if(event.keyCode == 123) {
-  return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
-  return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
-  return false;
-  }
-  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
-  return false;
-  }
-  if(e.ctrlKey && e.keyCode == 'CTRL'.charCodeAt(0)){
-    return false;
-  }
-  }
