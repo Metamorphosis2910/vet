a26359dc80ef6b59130a6abd217082f7a0eca2d2
Metamorphosis2910 <ggurvon@gmail.com>
2024-02-17 20:25:22 +0500

    Страница объявления
    Добавлена страница объявления, доработана на 60-70%

--- obyavlenie/js/js.js
+++ obyavlenie/js/js.js
@@ -1,37 +1,22 @@
-document.addEventListener('click', function (event) {
-  const burgerMenu = document.getElementById('navbarToggleExternalContent');
-  const burgerButton = document.querySelector('.navbar-toggler');
+//   document.addEventListener("contextmenu", function (e){
+//     e.preventDefault();
+// }, false);
 
-  // Если клик был вне бургер-меню и кнопки, то закрываем меню
-  if (!burgerMenu.contains(event.target) && !burgerButton.contains(event.target)) {
-    const isMenuOpen = burgerMenu.classList.contains('show');
-    
-    if (isMenuOpen) {
-      // Используем jQuery для закрытия бургер-меню
-      $(burgerButton).click();
-    }
-  }
-});
+// document.onkeydown = function(e) {
+//   if(event.keyCode == 123) {
+//   return false;
+//   }
+//   if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+//   return false;
+//   }
+//   if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+//   return false;
+//   }
+//   if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+//   return false;
+//   }
+//   if(e.ctrlKey && e.keyCode == 'CTRL'.charCodeAt(0)){
+//     return false;
+//   }
+//   }
 
-
-document.addEventListener("contextmenu", function (e){
-  e.preventDefault();
-}, false);
-
-document.onkeydown = function(e) {
-  if(event.keyCode == 123) {
-  return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
-  return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
-  return false;
-  }
-  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
-  return false;
-  }
-  if(e.ctrlKey && e.keyCode == 'CTRL'.charCodeAt(0)){
-    return false;
-  }
-  }
