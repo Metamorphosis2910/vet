dc43c214a26681d182f2441e097fb032d7069a17
Metamorphosis2910 <ggurvon@gmail.com>
2024-03-23 17:00:59 +0500

    Мобильная адаптация 4.0
    Регистрация и авторизация

--- autorization/js/js.js
+++ autorization/js/js.js
@@ -1,19 +1,4 @@
-document.addEventListener('click', function (event) {
-    const burgerMenu = document.getElementById('navbarToggleExternalContent');
-    const burgerButton = document.querySelector('.navbar-toggler');
-  
-    // Если клик был вне бургер-меню и кнопки, то закрываем меню
-    if (!burgerMenu.contains(event.target) && !burgerButton.contains(event.target)) {
-      const isMenuOpen = burgerMenu.classList.contains('show');
-      
-      if (isMenuOpen) {
-        // Используем jQuery для закрытия бургер-меню
-        $(burgerButton).click();
-      }
-    }
-  });
-  
-  
+
 //   document.addEventListener("contextmenu", function (e){
 //     e.preventDefault();
 // }, false);
